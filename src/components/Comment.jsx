986ca205c3ce1f7a197e6f51fabf224059b51c4d
Nneka Chukwuemeka <nneka.uchechukwu.chukwuemeka@gmail.com>
2023-01-19 14:58:35 +0100

    video playing

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,13 +1,29 @@
+import { useRef, useState } from 'react';
+
 // import components
 import CommentProfile from './CommentProfile.jsx';
 
-// import Image
-import cover from '../assets/cover.svg';
+// import Video
+import VideoBg from '../assets/homerental.mp4';
+
 // import react icons
 import { ImQuotesLeft } from 'react-icons/im';
-import { BsCircle } from 'react-icons/bs';
+import { BsCircle, BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
 
 function Comment() {
+  const videoRef = useRef();
+  const [playing, setPlaying] = useState(true);
+  const [shown, setShown] = useState(false);
+
+  const handlePlay = () => {
+    if (playing) {
+      setPlaying(false);
+      videoRef.current.pause();
+    } else {
+      setPlaying(true);
+      videoRef.current.play();
+    }
+  };
   return (
     <div className=" flex lg:justify-between bg-[#f4511e12] flex-col md:flex-row ">
       <div className="lg:ml-[129px] ml-2 pl-4  lg:max-w-[50%] lg:pt-[130px]  w-full pt-8 md:pt-6 pr-8  text-justify">
@@ -27,12 +43,35 @@ function Comment() {
           <BsCircle className="bg-[#F4511E]/70 text-[#F4511E]/70 rounded-full mr-2 cursor-pointer hover:bg-white" />
         </div>
       </div>
-      <div className="lg:max-w-[50%] w-full h-max-full md:order-last order-first">
-        <img
+      <div
+        className="lg:max-w-[50%] w-full h-max-full md:order-last order-first relative"
+        onMouseEnter={() => setShown(true)}
+        onMouseLeave={() => setShown(false)}
+      >
+        {/* <img
           src={cover}
           alt="beautiful home decor"
           className="w-full  object-cover"
+        /> */}
+        <video
+          src={VideoBg}
+          autoPlay
+          loop
+          className="object-cover w-full h-full"
+          ref={videoRef}
         />
+        {shown && (
+          <button
+            className="absolute left-[50%] top-[45%] "
+            onClick={handlePlay}
+          >
+            {playing ? (
+              <BsFillPlayFill className="text-red-500 text-[2rem] lg:text-[4rem]" />
+            ) : (
+              <BsPauseFill className="text-red-500 text-[2rem] lg:text-[4rem]" />
+            )}
+          </button>
+        )}
       </div>
     </div>
   );
